9168818c17bb966b97f24d97de2ab8522ccade65
Kevin Rowett <kevin@rowett.org>
2020-08-17 12:34:37 -0700

    reads values from meter

---
+++ ra/readRFA.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "bytes"
+    "fmt"
+    "net/http"
+	"io/ioutil"
+	"encoding/xml"
+)
+
+func parseMeterXML( bodyText []byte ) {
+	type Result struct {
+		// XMLName	xml.Name	`xml:"DeviceDetails"`
+		XMLName	xml.Name	`xml:"Device"`
+		HWaddr	string	`xml:"DeviceDetails>HardwareAddress"`
+		VarValues	[]string	`xml:"Components>Component>Variables>Variable>Value"`
+	}
+	v := Result{}
+	err := xml.Unmarshal(bodyText, &v)
+	if err != nil {
+		fmt.Printf("xml unmashall error: %v\n", err)
+		return
+	}
+	fmt.Printf("HW address %s\n",v.HWaddr)
+	fmt.Printf("Values: %v\n",v.VarValues)
+
+}
+
+func main() {
+	var err error
+    // or you can use []byte(`...`) and convert to Buffer later on
+    body := `<Command>
+    <Name>device_query</Name>
+    <DeviceDetails>
+        <HardwareAddress>0x0013500200a196eb </HardwareAddress>
+    </DeviceDetails>
+    <Components>
+        <Component>
+            <Name>Main</Name>
+                <Variables>
+                    <Variable>
+                        <Name>zigbee:InstantaneousDemand</Name>
+                    </Variable>
+                    <Variable>
+                        <Name>zigbee:CurrentSummationDelivered</Name>
+                    </Variable>
+                </Variables>
+        </Component>
+    </Components>
+</Command>`
+
+	// fmt.Println(body)
+    client := &http.Client{}
+    // build a new request, but not doing the POST yet
+    req, err := http.NewRequest("POST", "http://192.168.21.127/cgi-bin/post_manager/", bytes.NewBuffer([]byte(body)))
+    if err != nil {
+        fmt.Println(err)
+    }
+    req.Header.Add("Content-Type", "text/xml; charset=utf-8")
+	req.SetBasicAuth("006d60","4b0190726b0bbe37")
+    // now POST it
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+    }
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	parseMeterXML(bodyText)
+	s := string(bodyText)
+	// 's' is now a string version of an XML response
+	fmt.Println("XML formatted result:")
+	fmt.Println(s)
+	fmt.Println("---end---")
+}
+
