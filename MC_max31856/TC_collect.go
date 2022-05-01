45908b5fb1ff8f5a7f68739b5ef5030ef239f9ce
Kevin Rowett <kevin@rowett.org>
2022-05-01 14:21:44 -0700

    code added for multi-channel - untested

--- MC_max31856/TC_collect.go
+++ MC_max31856/TC_collect.go
@@ -26,27 +26,19 @@ type TCProbe struct {
 	name       string
 	ampPortNum int
 	cs_name    string
-	//	lastReading float32
-	active bool
+	active     bool
+	channel    max31856.MAX31856
 }
 
 var TCbyNum map[int]TCProbe
 
 var TCs [4]TCProbe
 
-// var TCamp_1 TCamp
-
-// var influxHostName string
-
 var dbClient influxdb2.Client
 var writeAPIx api.WriteAPIBlocking
 
-// var writeAPIx interface{}
 var configTitle string
 var MyDB string
-
-// var username string
-// var password string
 var testName string
 var ch0 max31856.MAX31856
 
@@ -78,11 +70,9 @@ var c Config
 
 // use influxDB to store TC readings
 func initDB() {
-	// const token = "sHMevZdU_7FoHVAnnt9jtCSrLqlwgvBautxWa8S-63cUyqsNsAdegQ8VFgNwhmBXl5MXwAo-q8iIipn824w5kg=="
-
 	// create new client with default option for server URL authenticate by token
 	dbClient = influxdb2.NewClient(c.Db.ServerURL, c.Db.AuthTKN)
-	// writeAPIx = dbClient.WriteAPIBlocking("kevin.rowett@xconn-tech.com", "TC")
+
 	// writeAPI expects bucket nam - we use DBName for bucket name
 	writeAPIx = dbClient.WriteAPIBlocking(c.Db.ORGName, c.Db.DBName)
 
@@ -92,15 +82,27 @@ func initTC() {
 	var spiClockSpeed int64 = 100000
 	// var ch0 max31856.MAX31856
 	var err error
-	devPathCh0 := "/dev/spidev0.0"
+	// devPathCh0 := "/dev/spidev0.0"
+	devPathChRoot := "/dev/spidev0."
 	timeoutPeriod := time.Second
 	fmt.Println("starting TC init")
-	ch0, err = max31856.Setup(devPathCh0, spiClockSpeed, 0, timeoutPeriod)
+	for i, _ := range TCs {
+		log.Printf("index:%d probe name:%s cs value:%s\n", i, TCs[i].name, TCs[i].cs_name)
+		if !TCs[i].active {
+			log.Printf("not active, skipping\n")
+			continue
+		}
+		devPathCh := devPathChRoot + TCs[i].cs_name
+		TCs[i].channel, err = max31856.Setup(devPathCh, spiClockSpeed, 0, timeoutPeriod)
+		if err != nil {
+			log.Printf("TC failed setup:%s %s\n", TCs[i].name, err)
+			TCs[i].active = false
+		} else {
+			log.Printf("spi channel init complete for:%s dev name:%s\n", TCs[i].name, devPathCh)
+		}
 
-	if err != nil {
-		fmt.Println(err)
 	}
-
+	// ch0, err = max31856.Setup(devPathCh0, spiClockSpeed, 0, timeoutPeriod)
 	fmt.Println("CJLF_WR")
 	fmt.Println(max31856.CJLF_WR)
 }
@@ -156,13 +158,10 @@ func loadProbeConfig() (err error) {
 	// enumerate over all the probes configured
 	for i, p := range c.Probe {
 		log.Printf("probe:%d, name:%s entry:%d", p.Num, p.Name, i)
-		t := TCbyNum[p.Num]
-		t.name = p.Name
-		t.ampPortNum = p.Num
-		t.cs_name = p.CS
-		t.active = p.Active
-		TCbyNum[p.Num] = t
-		// TCbyNum[p.Num].name = p.Name
+		TCs[i].ampPortNum = p.Num
+		TCs[i].name = p.Name
+		TCs[i].cs_name = p.CS
+		TCs[i].active = p.Active
 	}
 	return (nil)
 }
@@ -180,10 +179,6 @@ func loadConfig() (err error) {
 	configTitle = viper.GetString("title")
 	log.Printf("%s", configTitle)
 	testName = viper.GetString("test_name")
-	//	MyDB = viper.GetString("DBName")
-	//	influxHostName = viper.GetString("server")
-	//	username = viper.GetString("username")
-	//	password = viper.GetString("password")
 
 	if err := viper.Unmarshal(&c); err != nil {
 		log.Printf("couldn't read config: %s", err)
@@ -211,9 +206,14 @@ func main() {
 	log.Printf("*** MC_MAX31586 Reader %s starting ***", ourVersion)
 	initTC()
 	for {
-		tempC, _ = ch0.GetTempOnce()
-		insertReading(tempC, 0, TCbyNum[1].name)
-		log.Printf("TC reading:%f", tempC)
+		for i, _ := range TCs {
+			if !TCs[i].active {
+				continue
+			}
+			tempC, _ = TCs[i].channel.GetTempOnce()
+			insertReading(tempC, i, TCs[i].name)
+			log.Printf("TC reading:%f probe name:%s index:%d", tempC, TCs[i].name, i)
+		}
 		time.Sleep(time.Duration(c.Db.DelayTime) * time.Millisecond)
 	}
 }
