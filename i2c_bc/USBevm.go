20eb848bc3d109c382c6a987295a460487859f5e
Kevin Rowett <kevin@ironheartconsulting.com>
2023-09-09 20:34:17 -0700

    field breakout

--- i2c_bc/USBevm.go
+++ i2c_bc/USBevm.go
@@ -19,6 +19,8 @@ const modeReg byte = 0x03
 const typeReg byte = 0x04
 const verReg byte = 0x0f
 const devCap byte = 0x0d
+const bootStatus byte = 0x2D
+const devInfo byte = 0x2f
 
 var i2c *i2c_mod.I2C
 
@@ -27,7 +29,35 @@ var lg = logger.NewPackageLogger("main",
 	// logger.InfoLevel,
 )
 
-func fetchReg(addr byte, count int) {
+// decodeBS - decode boot status register - 5 bytes LE
+func decodeBS(buf []byte) {
+
+	var srcName string
+
+	fmt.Printf("           HW version:%x\n", buf[5])
+	ptchCfgSrc := (buf[4] & 0xe0) >> 5
+	switch ptchCfgSrc {
+	case 0:
+		srcName = "no config loaded"
+	case 1, 2, 3, 4, 7:
+		srcName = "Reserved"
+	case 5:
+		srcName = "EEPROM"
+	case 6:
+		srcName = "I2C"
+	}
+
+	fmt.Printf("  patch config source:%d\n", (buf[4]&0xe0)>>5) // bits 31:29
+	fmt.Printf("         patch config:%s\n", srcName)
+	fmt.Printf("thermal caused reboot:%d\n", (buf[3]&0x08)>>3) // bit 19
+	fmt.Printf("   patch download err:%d\n", (buf[2]&0x04)>>2) // bit 10
+	fmt.Printf("       EEPROM Present:%d\n", (buf[1]&0x08)>>3) // bit 3
+	fmt.Printf("            dead batt:%d\n", (buf[1]&0x04)>>2) // bit 2
+	fmt.Printf(" patch bundle hdr err:%d\n", buf[1]&0x01)      // bit 0
+
+}
+
+func fetchReg(addr byte, count int) []byte {
 	var err error
 	var buf []byte
 
@@ -35,7 +65,8 @@ func fetchReg(addr byte, count int) {
 	if err != nil {
 		fmt.Printf("error:%s\n", err)
 	}
-	fmt.Printf("reg:%x readcount:%d buf:%s\n", addr, count, buf)
+	// fmt.Printf("reg:%x readcount:%d buf:%s\n", addr, count, buf)
+	return buf
 
 }
 
@@ -47,13 +78,14 @@ func fetchRegv(addr byte, count int) {
 	if err != nil {
 		fmt.Printf("error:%s\n", err)
 	}
-	fmt.Printf("reg:%x readcount:%d buf:%v\n", addr, count, buf)
+	fmt.Printf("reg:%x readcount:%d buf:%x\n", addr, count, buf)
 
 }
 
 func main() {
 
 	var err error
+	var buf []byte
 
 	//	cmd line flags
 	debugFlag := flag.Bool("debug", false, "control debugging output")
@@ -78,9 +110,19 @@ func main() {
 		logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
 	}
 
-	fetchReg(modeReg, 4)
-	fetchReg(typeReg, 4)
-	fetchRegv(verReg, 4)
-	fetchRegv(devCap, 4)
+	buf = fetchReg(modeReg, 4)
+	fmt.Printf("mode:%s\n", buf)
+	buf = fetchReg(typeReg, 4)
+	fmt.Printf("type:%s\n", buf)
+	buf = fetchReg(verReg, 4)
+	fmt.Printf("ver string:%x\n", buf)
+	fmt.Printf("ver: %02X%02X.%02X.%02X\n", buf[4], buf[3], buf[2], buf[1])
+	buf = fetchReg(devCap, 4)
+	fmt.Printf("device capabilities:%x\n", buf)
+	buf = fetchReg(bootStatus, 5)
+	fmt.Printf("boot status:%x\n", buf[1:6])
+	decodeBS(buf)
+	buf = fetchReg(devInfo, 40)
+	fmt.Printf("device info:%s\n", buf)
 
 }
