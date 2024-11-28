8e3aacf4d225d9d3a06d38696b3b6f12d94dbcde
Kevin Rowett <kevin@ironheartconsulting.com>
2024-11-28 13:16:24 -0800

    basic c/s sending pkts

--- rawIP/rawIP.go
+++ rawIP/rawIP.go
@@ -16,6 +16,9 @@ var mode = flag.String("mode", "client", "client | server mode")
 var serIP = flag.String("serverIP", "127.0.0.1", "ip adddress of server process")
 var blkCnt = flag.Int("cnt", 10, "Count of block to send")
 var blksize = flag.Int("bsize", 1024, "size of each block")
+var debug = flag.Bool("debug", false, "debug true|false")
+
+var protocol = "61"
 
 // rawP -mode server | client -ServerIP <IP addr> -cnt <cnt of blocks to send? -bsize <size of each block>
 
@@ -29,59 +32,74 @@ func main() {
 
 	flag.Parse()
 
-	protocol := "61"
+	fmt.Println("raw IP xfer. Version", "V0.6")
 	if *mode == "server" {
-		fmt.Printf("server mode\n")
-		// netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
-		// conn, _ := net.ListenIP("ip4:"+protocol, netaddr)
-		priorBlkNum := -1
-		conn, _ := net.ListenIP("ip4:"+protocol, nil)
-		buf := make([]byte, 1024)
-		for {
-			n, _, err := conn.ReadFrom(buf)
-			if err != nil {
-				fmt.Println("server read error", err)
-				os.Exit(4)
-			}
-			fmt.Printf("Read len:%d\n% X\n", n, buf[0:20])
-			blknum := binary.LittleEndian.Uint16(buf[0:2])
-			if priorBlkNum+1 != int(blknum) {
-				fmt.Printf("out of seq. Expected:%d\n", priorBlkNum+1)
-			}
-			fmt.Printf("seq num:%d\n", blknum)
-			priorBlkNum = priorBlkNum + 1
-			ieeeChecksum := crc32.ChecksumIEEE(buf[6 : n-1])
-			fmt.Printf("crc32: % X\n", ieeeChecksum)
-			bufCkSum := binary.BigEndian.Uint32(buf[2:6])
-			fmt.Printf("check sum from client side:% X\n", bufCkSum)
-			if bufCkSum != ieeeChecksum {
-				fmt.Printf("client checksum differs from server computed\n")
-			}
-		}
+		serverMode()
 	} else { //assume client mode
-		fmt.Printf("client mode. IP:%s\n", *serIP)
-		conn, err := net.Dial("ip4:"+protocol, *serIP)
-		if err != nil {
-			panic(err)
-		}
-		buf := make([]byte, *blksize)
-		rand.Seed(time.Now().UnixNano())
-		// pattern := []byte{0xde, 0xad, 0xbe, 0xef}
-
-		for blockNum := 0; blockNum < *blkCnt; blockNum++ {
-			blknum := make([]byte, 2)
-			binary.LittleEndian.PutUint16(blknum, uint16(blockNum))
-			rand.Read(buf)
-			copy(buf, blknum)
-			ieeeChecksum := crc32.ChecksumIEEE(buf[6 : *blksize-1])
-			crcByte := make([]byte, 4)
-			binary.BigEndian.PutUint32(crcByte, ieeeChecksum)
-			copy(buf[2:6], crcByte)
-			conn.Write(buf)
-			fmt.Printf("block interation:%d block number:%v block number type:%T\n", blockNum, blknum, blknum)
-			fmt.Printf("crc32:% X type:%T\n", ieeeChecksum, ieeeChecksum)
-			fmt.Printf("Len buf:%d\n", len(buf))
-		}
-
+		clientMode()
 	}
+}
+
+func serverMode() {
+	fmt.Printf("server mode\n")
+	// netaddr, _ := net.ResolveIPAddr("ip4", "127.0.0.1")
+	// conn, _ := net.ListenIP("ip4:"+protocol, netaddr)
+	priorBlkNum := -1
+	conn, _ := net.ListenIP("ip4:"+protocol, nil)
+	buf := make([]byte, 1024)
+	for {
+		n, _, err := conn.ReadFrom(buf)
+		if err != nil {
+			fmt.Println("server read error", err)
+			os.Exit(4)
+		}
+		fmt.Print(".")
+		if *debug {
+			fmt.Printf("Read len:%d\n% X\n", n, buf[0:20])
+		}
+		blknum := binary.LittleEndian.Uint16(buf[0:2])
+		if priorBlkNum+1 != int(blknum) {
+			fmt.Printf("out of seq. Expected:%d\n", priorBlkNum+1)
+		}
+		if *debug {
+			fmt.Printf("seq num:%d\n", blknum)
+		}
+		priorBlkNum = priorBlkNum + 1
+		ieeeChecksum := crc32.ChecksumIEEE(buf[6 : n-1])
+		bufCkSum := binary.BigEndian.Uint32(buf[2:6])
+		if *debug {
+			fmt.Printf("crc32: % X\n", ieeeChecksum)
+			fmt.Printf("check sum from client side:% X\n", bufCkSum)
+		}
+		if bufCkSum != ieeeChecksum {
+			fmt.Printf("client checksum differs from server computed\n")
+		}
+	}
+}
+
+func clientMode() {
+	fmt.Printf("client mode. IP:%s\n", *serIP)
+	conn, err := net.Dial("ip4:"+protocol, *serIP)
+	if err != nil {
+		panic(err)
+	}
+	buf := make([]byte, *blksize)
+	rand.Seed(time.Now().UnixNano())
+	// pattern := []byte{0xde, 0xad, 0xbe, 0xef}
+
+	for blockNum := 0; blockNum < *blkCnt; blockNum++ {
+		blknum := make([]byte, 2)
+		binary.LittleEndian.PutUint16(blknum, uint16(blockNum))
+		rand.Read(buf)
+		copy(buf, blknum)
+		ieeeChecksum := crc32.ChecksumIEEE(buf[6 : *blksize-1])
+		crcByte := make([]byte, 4)
+		binary.BigEndian.PutUint32(crcByte, ieeeChecksum)
+		copy(buf[2:6], crcByte)
+		conn.Write(buf)
+		fmt.Printf("block interation:%d block number:%v block number type:%T\n", blockNum, blknum, blknum)
+		fmt.Printf("crc32:% X type:%T\n", ieeeChecksum, ieeeChecksum)
+		fmt.Printf("Len buf:%d\n", len(buf))
+	}
+
 }
